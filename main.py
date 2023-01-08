d3aa7eaab75012c9b2f340c89f233491f6b01fcc
Shera Adams <110789514+sheraadams@users.noreply.github.com>
2023-01-08 11:16:24 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ dark= "#323E42"
 purple= "#7954A1"
 back = "#278ED5"
 contrast = "#AFFF26"
+
+# window dimesions and color
 window = Tk()
-window.geometry("720x720")  # was 720 x 420
+window.geometry("720x1000")  # was 720 x 420
 window.configure(bg=purple)
 window.title("RSA Encryption and Decryption Generator")
 
@@ -21,8 +23,40 @@ def start():
     if __name__== '__main__':
         option = "menu"
 
+def toSpace():
+    # get input to convert comma to space
+    toSpaceMessage = uinput7.get("1.0", "end-1c").lower()
+
+    for i in range(len(toSpaceMessage)):
+        toSpaceMessage= toSpaceMessage.replace(",", ' ')
+
+    print(toSpaceMessage)
+    chatb4.delete("1.0", END)
+    chatb4.insert(END, toSpaceMessage)
+
 # function to encode
 def encrypt():
+
+    # get input from textbox for value to assign to space
+    space = uinput4.get("1.0", "end-1c").lower()
+    # get input from textbox for value to assign to period
+    period = uinput6.get("1.0", "end-1c").lower()
+
+
+    # convert user input string to a digit
+    if space.isdigit():
+        int(space)
+    if period.isdigit():
+        int(period)
+
+    notifyInvalid = ""
+    # check that variable are unique
+    if space == period:
+        notifyInvalid = "Invalid input please assign space and period to unique values."
+
+    chatb3.delete("1.0", END)
+    chatb3.insert(END, notifyInvalid)
+
     # define variables
     p  = 3
     q = 11
@@ -92,10 +126,10 @@ def encrypt():
             messagelist1[i] = '25'
         if messagelist1[i] == 'z':
             messagelist1[i] = '26'
-        if messagelist1[i] == ' ':
-            messagelist1[i] = '32'
+        if messagelist1[i] == " ":
+            messagelist1[i] = space
         if messagelist1[i] == '.':
-            messagelist1[i] = '28'
+            messagelist1[i] = period
 
     # print to console convert to string, strip commas and format appropriately
     print(messagelist1)
@@ -120,63 +154,7 @@ def encrypt():
         encrypt1[i] = (encrypt1[i] ** e) % n
 
     # convert numeric to string and format single digits to be preceded by 0s
-    for i in range(len(encrypt1)):
-        if encrypt1[i] == 1:
-            encrypt1[i] = "01"
-        if encrypt1[i] == 2:
-            encrypt1[i] = "02"
-        if encrypt1[i] == 3:
-            encrypt1[i] = "03"
-        if encrypt1[i] == 4:
-            encrypt1[i] = "04"
-        if encrypt1[i] == 5:
-            encrypt1[i] = "05"
-        if encrypt1[i] == 6:
-            encrypt1[i] = "06"
-        if encrypt1[i] == 7:
-            encrypt1[i] = "07"
-        if encrypt1[i] == 8:
-            encrypt1[i] = "08"
-        if encrypt1[i] == 9:
-            encrypt1[i] = "09"
-        if encrypt1[i] == 10:
-            encrypt1[i] = "10"
-        if encrypt1[i] == 11:
-            encrypt1[i] = "11"
-        if encrypt1[i] == 12:
-            encrypt1[i] = "12"
-        if encrypt1[i] == 13:
-            encrypt1[i] = "13"
-        if encrypt1[i] == 14:
-            encrypt1[i] = "14"
-        if encrypt1[i] == 15:
-            encrypt1[i] = "15"
-        if encrypt1[i] == 16:
-            encrypt1[i] = "16"
-        if encrypt1[i] == 17:
-            encrypt1[i] = "17"
-        if encrypt1[i] == 18:
-            encrypt1[i] = "18"
-        if encrypt1[i] == 19:
-            encrypt1[i] = "19"
-        if encrypt1[i] == 20:
-            encrypt1[i] = "20"
-        if encrypt1[i] == 21:
-            encrypt1[i] = "21"
-        if encrypt1[i] == 22:
-            encrypt1[i] = "22"
-        if encrypt1[i] == 23:
-            encrypt1[i] = "23"
-        if encrypt1[i] == 24:
-            encrypt1[i] = "24"
-        if encrypt1[i] == 25:
-            encrypt1[i] = "25"
-        if encrypt1[i] == 26:
-            encrypt1[i] = "26"
-        if encrypt1[i] == 28:
-            encrypt1[i] = "28"
-        if encrypt1[i] == 32:
-            encrypt1[i] = "32"
+    en = [str(x) for x in encrypt1]
 
 
     en = str(encrypt1)
@@ -195,6 +173,26 @@ def encrypt():
 
 # function to decrypt
 def decode():
+
+    # get input from textbox for value to assign to space
+    space = uinput4.get("1.0", "end-1c").lower()
+    # get input from textbox for value to assign to period
+    period = uinput6.get("1.0", "end-1c").lower()
+
+    if space.isdigit():
+        int(space)
+    if period.isdigit():
+        int(period)
+
+    notifyInvalid = ""
+    # check that values are unique
+    if space == period:
+        notifyInvalid = "Invalid input please assign space and period to unique values."
+
+    chatb3.delete("1.0", END)
+    chatb3.insert(END, notifyInvalid)
+
+
     # get input from the gui text entry box
     word2 = uinput3.get("1.0", "end-1c").lower()
     decry = word2.lower()
@@ -221,6 +219,7 @@ def decode():
         decry = decry.replace('08', '8')
         decry = decry.replace('09', '9')
     decmessage = []
+
     decry2 = [int(item) for item in decry.split(',') if item.isdigit()]
 
     # iterate through list applying rsa decryption algorithm and append decrypted elements to new list
@@ -228,7 +227,6 @@ def decode():
         i = (i ** d) % n
         print(i, end=', ')
         decmessage.append(i)
-
     # iterate through list and assign numeric to alpha 1=a, b =2 etc
     for i in range(len(decmessage)):
         if decmessage[i] == 1:
@@ -283,12 +281,13 @@ def decode():
             decmessage[i] = 'y'
         if decmessage[i] == 26:
             decmessage[i] = 'z'
-        if decmessage[i] == 28:
-            decmessage[i] = '.'
-        if decmessage[i] == 32:
-            decmessage[i] = ' '
+        if decmessage[i] == int(space):
+            decmessage[i] = " "
+        if decmessage[i] == int(period):
+            decmessage[i] = "."
     j = 1
     decstr = str(decmessage)
+
 
     # strip unwanted characters
     while j == 1:
@@ -319,41 +318,104 @@ def decode():
 path = '2.png'
 img= ImageTk.PhotoImage(Image.open(path))
 
+# pack header image panel
 panel = tk.Label(window, fg= purple, image = img, borderwidth=0, highlightthickness=0)
 panel.image = img
 panel.pack()
-label1 = Label(window, bg= purple, fg= contrast, font='Helvetica 10 bold', text="Key: p=3, q=11, e=3. Encryption: x^e %n Decryption: x^(10-e) %n", pady=5)
-label1.pack()
+
+# window to get input for number to assign to space
+label4 = Label(window, bg= purple, fg= contrast, font='Helvetica 10 bold', text="Assign space to a unique number (27-32)", pady=8)
+label4.pack()
+# value
+uinput_value4 = StringVar(window)
+# entry
+uinput4 = Text(window, height=1, width=65, bg=lpurple)
+uinput4.pack()
+
+
+# window to get input for number to assign to period
+# label
+label6 = Label(window, bg= purple, fg= contrast, font='Helvetica 10 bold', text="Assign period (27-32)", pady=8)
+label6.pack()
+# entry value
+uinput_value6 = StringVar(window)
+#entry
+uinput6 = Text(window, height=1, width=65, bg=lpurple)
+uinput6.pack()
+
+# label enter message to encrypt
 label3 = Label(window, bg= purple, fg= contrast, font='Helvetica 10 bold', text="Encrypt: Enter a message to encrypt", pady=8)
 label3.pack()
+# value
 uinput_value = StringVar(window)
+# input window
 uinput = Text(window, height=5, width=65, bg=lpurple)
 uinput.pack()
 
+
+# text window for output message encrypted
 label4 = Label(window, bg= purple, fg= lpurple, font='Helvetica 9 bold', text="Encrypted message:", pady=5)
 label4.pack()
+# output entry box
 entry4 = Text(window, height=5, width=65, bg=lpurple)
 entry4.pack()
+# labels
 label2 = Label(window, bg= purple, fg= lpurple, font='Helvetica 9 bold', text="", pady=0)
 label2.pack()
 button02 = Button(window, bg= dpurple, fg=lpurple, height=1, width=10, text="Encrypt",command=lambda: encrypt())
 button02.pack()
 
 
+# window for text entry for message to decrypt
+# label
 label2 = Label(window, bg= purple, fg= contrast, font='Helvetica 10 bold', text="Decrypt: Enter a message to decrypt", pady=8)
 label2.pack()
+#value
 uinput_value3 = StringVar(window)
+# entry box
 uinput3 = Text(window, height=5, width=65, bg=lpurple)
 uinput3.pack()
+# label
 lab5 = Label(window, bg= purple, fg= lpurple, font='Helvetica 9 bold', text="Decrypted message:", pady=5)
 lab5.pack()
+# output entry box
 chatb3 = Text(window, height=5,width=65, bg=lpurple)
-
 chatb3.pack()
+
+
+
+#Label
 lab6 = Label(window, bg= purple, fg= lpurple, font='Helvetica 9 bold', text="", pady=0)
 lab6.pack()
+# Decrypt button
 button3 = Button(window, bg= dpurple, fg=lpurple, height=1, width=10, text="Decrypt",command=lambda: decode())
 button3.pack()
+
+
+# window for output comma to space
+# label
+label7 = Label(window, bg= purple, fg= contrast, font='Helvetica 10 bold', text="To convert a comma separated string enter it below:", pady=8)
+label7.pack()
+uinput_value7 = StringVar(window)
+uinput7 = Text(window, height=2.5, width=65, bg=lpurple)
+uinput7.pack()
+# label
+lab5 = Label(window, bg= purple, fg= lpurple, font='Helvetica 9 bold', text="Comma converted to space:", pady=5)
+lab5.pack()
+# window for output comma to space message
+chatb4 = Text(window, height=2.5,width=65, bg=lpurple)
+chatb4.pack()
+#label for spacing
+lab9 = Label(window, bg= purple, fg= lpurple, font='Helvetica 9 bold', text="", pady=0)
+lab9.pack()
+# Comma to space button
+button4 = Button(window, bg= dpurple, fg=lpurple, height=1, width=10, text="to Space",command=lambda: toSpace())
+button4.pack()
+
+
+# label
+label1 = Label(window, bg= purple, fg= contrast, font='Helvetica 10 bold', text="Using Key: p=3, q=11, e=3. Encryption: x^e %n Decryption: x^(10-e) %n", pady=5)
+label1.pack()
 
 
 window.mainloop()
