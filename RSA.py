0df005e004132feeb5e5eaf8dc62d260a9e4259b
Shera Adams <110789514+sheraadams@users.noreply.github.com>
2023-05-18 06:50:36 -0400

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -2,9 +2,9 @@ import statistics
 from tkinter import*
 import math
 import tkinter as tk
-
 from PIL import Image, ImageTk
 import time
+
 #colors
 dpurple = "#C55FFC"
 lpurple= "#EFDCF9"
@@ -34,73 +34,73 @@ def encrypt():
     d = 10-p
 
     # get input from textbox and convert to list, print to console
-    elmessage = uinput.get("1.0", "end-1c").lower()
-    messagelist1 = list(elmessage)
-    print(messagelist1)
+    messageInput = uinput.get("1.0", "end-1c").lower()
+    messagelist = list(messageInput)
+    print(messagelist)
 
     # convert alpha to numeric a = 1, b = 2 etc
-    for i in range(len(messagelist1)):
-        if messagelist1[i] == 'a':
-            messagelist1[i] = '1'
-        if messagelist1[i] == 'b':
-            messagelist1[i] = '2'
-        if messagelist1[i] == 'c':
-            messagelist1[i] = '3'
-        if messagelist1[i] == 'd':
-            messagelist1[i] = '4'
-        if messagelist1[i] == 'e':
-            messagelist1[i] = '5'
-        if messagelist1[i] == 'f':
-            messagelist1[i] = '6'
-        if messagelist1[i] == 'g':
-            messagelist1[i] = '7'
-        if messagelist1[i] == 'h':
-            messagelist1[i] = '8'
-        if messagelist1[i] == 'i':
-            messagelist1[i] = '9'
-        if messagelist1[i] == 'j':
-            messagelist1[i] = '10'
-        if messagelist1[i] == 'k':
-            messagelist1[i] = '11'
-        if messagelist1[i] == 'l':
-            messagelist1[i] = '12'
-        if messagelist1[i] == 'm':
-            messagelist1[i] = '13'
-        if messagelist1[i] == 'n':
-            messagelist1[i] = '14'
-        if messagelist1[i] == 'o':
-            messagelist1[i] = '15'
-        if messagelist1[i] == 'p':
-            messagelist1[i] = '16'
-        if messagelist1[i] == 'q':
-            messagelist1[i] = '17'
-        if messagelist1[i] == 'r':
-            messagelist1[i] = '18'
-        if messagelist1[i] == 's':
-            messagelist1[i] = '19'
-        if messagelist1[i] == 't':
-            messagelist1[i] = '20'
-        if messagelist1[i] == 'u':
-            messagelist1[i] = '21'
-        if messagelist1[i] == 'v':
-            messagelist1[i] = '22'
-        if messagelist1[i] == 'w':
-            messagelist1[i] = '23'
-        if messagelist1[i] == 'x':
-            messagelist1[i] = '24'
-        if messagelist1[i] == 'y':
-            messagelist1[i] = '25'
-        if messagelist1[i] == 'z':
-            messagelist1[i] = '26'
-        if messagelist1[i] == ' ':
-            messagelist1[i] = '32'
-        if messagelist1[i] == '.':
-            messagelist1[i] = '28'
+    for i in range(len(messagelist)):
+        if messagelist[i] == 'a':
+            messagelist[i] = '1'
+        if messagelist[i] == 'b':
+            messagelist[i] = '2'
+        if messagelist[i] == 'c':
+            messagelist[i] = '3'
+        if messagelist[i] == 'd':
+            messagelist[i] = '4'
+        if messagelist[i] == 'e':
+            messagelist[i] = '5'
+        if messagelist[i] == 'f':
+            messagelist[i] = '6'
+        if messagelist[i] == 'g':
+            messagelist[i] = '7'
+        if messagelist[i] == 'h':
+            messagelist[i] = '8'
+        if messagelist[i] == 'i':
+            messagelist[i] = '9'
+        if messagelist[i] == 'j':
+            messagelist[i] = '10'
+        if messagelist[i] == 'k':
+            messagelist[i] = '11'
+        if messagelist[i] == 'l':
+            messagelist[i] = '12'
+        if messagelist[i] == 'm':
+            messagelist[i] = '13'
+        if messagelist[i] == 'n':
+            messagelist[i] = '14'
+        if messagelist[i] == 'o':
+            messagelist[i] = '15'
+        if messagelist[i] == 'p':
+            messagelist[i] = '16'
+        if messagelist[i] == 'q':
+            messagelist[i] = '17'
+        if messagelist[i] == 'r':
+            messagelist[i] = '18'
+        if messagelist[i] == 's':
+            messagelist[i] = '19'
+        if messagelist[i] == 't':
+            messagelist[i] = '20'
+        if messagelist[i] == 'u':
+            messagelist[i] = '21'
+        if messagelist[i] == 'v':
+            messagelist[i] = '22'
+        if messagelist[i] == 'w':
+            messagelist[i] = '23'
+        if messagelist[i] == 'x':
+            messagelist[i] = '24'
+        if messagelist[i] == 'y':
+            messagelist[i] = '25'
+        if messagelist[i] == 'z':
+            messagelist[i] = '26'
+        if messagelist[i] == ' ':
+            messagelist[i] = '32'
+        if messagelist[i] == '.':
+            messagelist[i] = '28'
 
     # print to console convert to string, strip commas and format appropriately
-    print(messagelist1)
-    print("messagelist1 string")
-    tempmessagestr = str(messagelist1)
+    print(messagelist)
+    print("messagelist string")
+    tempmessagestr = str(messagelist)
     print(tempmessagestr)
     print("temp message string")
 
@@ -115,71 +115,15 @@ def encrypt():
     print("encrypted string")
 
     # evaluate string as numeric and make a list, then use RSA algorithm calculation on each element
-    encrypt1 = [eval(i) for i in encrypted]
-    for i in range(len(encrypt1)):
-        encrypt1[i] = (encrypt1[i] ** e) % n
+    encryptlist = [eval(i) for i in encrypted]
+    for i in range(len(encryptlist)):
+        encryptlist[i] = (encryptlist[i] ** e) % n
 
-    # convert numeric to string and format single digits to be preceded by 0s
-    for i in range(len(encrypt1)):
-        if encrypt1[i] == 1:
-            encrypt1[i] = "01"
-        if encrypt1[i] == 2:
-            encrypt1[i] = "02"
-        if encrypt1[i] == 3:
-            encrypt1[i] = "03"
-        if encrypt1[i] == 4:
-            encrypt1[i] = "04"
-        if encrypt1[i] == 5:
-            encrypt1[i] = "05"
-        if encrypt1[i] == 6:
-            encrypt1[i] = "06"
-        if encrypt1[i] == 7:
-            encrypt1[i] = "07"
-        if encrypt1[i] == 8:
-            encrypt1[i] = "08"
-        if encrypt1[i] == 9:
-            encrypt1[i] = "09"
-        if encrypt1[i] == 10:
-            encrypt1[i] = "10"
-        if encrypt1[i] == 11:
-            encrypt1[i] = "11"
-        if encrypt1[i] == 12:
-            encrypt1[i] = "12"
-        if encrypt1[i] == 13:
-            encrypt1[i] = "13"
-        if encrypt1[i] == 14:
-            encrypt1[i] = "14"
-        if encrypt1[i] == 15:
-            encrypt1[i] = "15"
-        if encrypt1[i] == 16:
-            encrypt1[i] = "16"
-        if encrypt1[i] == 17:
-            encrypt1[i] = "17"
-        if encrypt1[i] == 18:
-            encrypt1[i] = "18"
-        if encrypt1[i] == 19:
-            encrypt1[i] = "19"
-        if encrypt1[i] == 20:
-            encrypt1[i] = "20"
-        if encrypt1[i] == 21:
-            encrypt1[i] = "21"
-        if encrypt1[i] == 22:
-            encrypt1[i] = "22"
-        if encrypt1[i] == 23:
-            encrypt1[i] = "23"
-        if encrypt1[i] == 24:
-            encrypt1[i] = "24"
-        if encrypt1[i] == 25:
-            encrypt1[i] = "25"
-        if encrypt1[i] == 26:
-            encrypt1[i] = "26"
-        if encrypt1[i] == 28:
-            encrypt1[i] = "28"
-        if encrypt1[i] == 32:
-            encrypt1[i] = "32"
+    # convert numeric to string and format single digits to be preceded by 0's
+    for i in range(len(encryptlist)):
+        encryptlist[i] = "{:02}".format(encryptlist[i])
 
-
-    en = str(encrypt1)
+    en = str(encryptlist)
     print(en)
 
     # format and send encrypted and formatted message to the gui
@@ -193,11 +137,11 @@ def encrypt():
     entry4.delete("1.0", END)
     entry4.insert(END, en)
 
-# function to decrypt
+# function to decodedpt
 def decode():
     # get input from the gui text entry box
     word2 = uinput3.get("1.0", "end-1c").lower()
-    decry = word2.lower()
+    decoded = word2.lower()
 
     # initialize variables
     p  = 3
@@ -210,83 +154,35 @@ def decode():
     d = 10-p
 
     # remove leading zeros and evaluate as digits
-    for i in range(len(decry)):
-        decry = decry.replace('01', '1')
-        decry = decry.replace('02', '2')
-        decry = decry.replace('03', '3')
-        decry = decry.replace('04', '4')
-        decry = decry.replace('05', '5')
-        decry = decry.replace('06', '6')
-        decry = decry.replace('07', '7')
-        decry = decry.replace('08', '8')
-        decry = decry.replace('09', '9')
+    for i in range(len(decoded)):
+        decoded = decoded.replace('01', '1')
+        decoded = decoded.replace('02', '2')
+        decoded = decoded.replace('03', '3')
+        decoded = decoded.replace('04', '4')
+        decoded = decoded.replace('05', '5')
+        decoded = decoded.replace('06', '6')
+        decoded = decoded.replace('07', '7')
+        decoded = decoded.replace('08', '8')
+        decoded = decoded.replace('09', '9')
     decmessage = []
-    decry2 = [int(item) for item in decry.split(',') if item.isdigit()]
+    decodedtemp = [int(item) for item in decoded.split(',') if item.isdigit()]
 
     # iterate through list applying rsa decryption algorithm and append decrypted elements to new list
-    for i in decry2:
+    for i in decodedtemp:
         i = (i ** d) % n
         print(i, end=', ')
         decmessage.append(i)
 
     # iterate through list and assign numeric to alpha 1=a, b =2 etc
+    mapping = {
+        1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j',
+        11: 'k', 12: 'l', 13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's',
+        20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y', 26: 'z', 28: '.', 32: ' '
+    }
+
     for i in range(len(decmessage)):
-        if decmessage[i] == 1:
-            decmessage[i] = 'a'
-        if decmessage[i] == 2:
-            decmessage[i] = 'b'
-        if decmessage[i] == 3:
-            decmessage[i] = 'c'
-        if decmessage[i] == 4:
-            decmessage[i] = 'd'
-        if decmessage[i] == 5:
-            decmessage[i] = 'e'
-        if decmessage[i] == 6:
-            decmessage[i] = 'f'
-        if decmessage[i] == 7:
-            decmessage[i] = 'g'
-        if decmessage[i] == 8:
-            decmessage[i] = 'h'
-        if decmessage[i] == 9:
-            decmessage[i] = 'i'
-        if decmessage[i] == 10:
-            decmessage[i] = 'j'
-        if decmessage[i] == 11:
-            decmessage[i] = 'k'
-        if decmessage[i] == 12:
-            decmessage[i] = 'l'
-        if decmessage[i] == 13:
-            decmessage[i] = 'm'
-        if decmessage[i] == 14:
-            decmessage[i] = 'n'
-        if decmessage[i] == 15:
-            decmessage[i] = 'o'
-        if decmessage[i] == 16:
-            decmessage[i] = 'p'
-        if decmessage[i] == 17:
-            decmessage[i] = 'q'
-        if decmessage[i] == 18:
-            decmessage[i] = 'r'
-        if decmessage[i] == 19:
-            decmessage[i] = 's'
-        if decmessage[i] == 20:
-            decmessage[i] = 't'
-        if decmessage[i] == 21:
-            decmessage[i] = 'u'
-        if decmessage[i] == 22:
-            decmessage[i] = 'v'
-        if decmessage[i] == 23:
-            decmessage[i] = 'w'
-        if decmessage[i] == 24:
-            decmessage[i] = 'x'
-        if decmessage[i] == 25:
-            decmessage[i] = 'y'
-        if decmessage[i] == 26:
-            decmessage[i] = 'z'
-        if decmessage[i] == 28:
-            decmessage[i] = '.'
-        if decmessage[i] == 32:
-            decmessage[i] = ' '
+        if decmessage[i] in mapping:
+            decmessage[i] = mapping[decmessage[i]]
     j = 1
     decstr = str(decmessage)
 
